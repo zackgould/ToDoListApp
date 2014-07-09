1a45cc7318dbab73cb123b6f80b9839f7168c5a1
zackgould <goulrz13@wfu.edu>
2014-07-09 13:26:28 -0600

    Updated using sessions and List.cs class.

---
+++ ToDoWebsite/ToDo/List.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace ToDoWebsite.ToDo
+{
+    public class List
+    {
+        public List<string> myList = new List<string>();
+
+        public void add(string item)
+        {
+            myList.Add(item);
+        }
+
+        public void modify(string item, string task)
+        {
+            var index = myList.IndexOf(item);
+            myList.Remove(item);
+            myList.Insert(index, task);
+        }
+        public void delete(string item)
+        {
+            myList.Remove(item);
+        }
+
+        public void sort(string[] order)
+        {
+            myList.Clear();
+            myList.AddRange(order);
+        }
+
+    }
+}
+
--- ToDoWebsite/ToDo/ToDoListEndpoint.cs
+++ ToDoWebsite/ToDo/ToDoListEndpoint.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Linq;
+﻿using System.Collections.Generic;
 using FubuMVC.Core.Continuations;
-using System.IO;
 using FubuMVC.Core.Runtime;
 
 namespace ToDoWebsite.ToDo
@@ -9,123 +7,102 @@ namespace ToDoWebsite.ToDo
 
     public class ToDoEndpoint
     {
-        private readonly IFubuRequest _fubuRequest;
+        private readonly ISessionState _session;
         private const string path = @"C:\Users\zackgoul\Desktop\ExtendHealth\C#\ToDo\ToDo.txt";
 
-        public ToDoEndpoint(IFubuRequest fubuRequest)
+        public ToDoEndpoint(ISessionState session)
         {
-            _fubuRequest = fubuRequest;
+            _session = session;
         }
 
         public ToDoListViewModel get_To(ToDoListInputModel inputModel)
         {
-            //Create new file unless one already exists
-            if (!File.Exists(path))
+            var toDoList = _session.Get<List>();
+
+            if (toDoList == null)
             {
-                // Create a file to write to. 
-                string createText = String.Empty;
-                File.WriteAllText(path, createText);
+                toDoList = new List();
+                _session.Set(toDoList);
             }
-            var toDoLines = File.ReadAllLines(path);
 
-            // this...
-            //var viewModel = new ToDoListViewModel();
-            //viewModel.ToDoList = fileText;
-
-            // is the same as this...
-            var viewModel = new ToDoListViewModel { ToDoList = toDoLines };
+            var viewModel = new ToDoListViewModel { ToDoList = toDoList.myList };
 
             return viewModel;
         }
 
         public FubuContinuation post_To(ToDoListPostInputModel inputModel)
         {
-            var item = inputModel.DeleteItem;
-            var linesToAdd = File.ReadAllLines(path).ToList();
-            
-            if (!linesToAdd.Contains(item))
-            {
-                // This text is always added, making the file longer over time 
-                string appendText = item + Environment.NewLine;
-                File.AppendAllText(path, appendText);
-            }
+            var item = inputModel.AddItem;
+
+            var toDoList = _session.Get<List>();
+
+            toDoList.add(item);
+
             return FubuContinuation.RedirectTo<ToDoListInputModel>();
         }
 
-        public FubuContinuation post_Removed(ToDoListPostInputModel inputModel)
+        public FubuContinuation post_Removed(ToDoListDeletePostInputModel inputModel)
         {
-                var left = inputModel.DeleteItem;
-           
-                var tempFile = Path.GetTempFileName();
+            var left = inputModel.DeleteItem;
 
-                var linesToKeep = File.ReadLines(path).Where(l => l != left);
-
-                File.WriteAllLines(tempFile, linesToKeep);
-
-                File.Delete(path);
-                File.Move(tempFile, path);
-            
-           
+            var toDoList = _session.Get<List>();
+            toDoList.delete(left);
 
             return FubuContinuation.RedirectTo<ToDoListInputModel>();
         }
 
-        public FubuContinuation post_Edited(ToDoListPostInputModel inputModel)
+        public FubuContinuation post_Edited(ToDoListEditPostInputModel inputModel)
         {
             var rightleft = inputModel.CompareItem;
             var right = inputModel.ModItem;
-            var linesToReplace = File.ReadAllLines(path).ToList();
 
-            if (!linesToReplace.Contains(right))
-            {
-               
-                var tempFile = Path.GetTempFileName();
-                var index = linesToReplace.IndexOf(rightleft);
-                linesToReplace.RemoveAt(index);
-                linesToReplace.Insert(index, right);
-                File.WriteAllLines(tempFile, linesToReplace);
-                File.Delete(path);
-                File.Move(tempFile, path);
-            }
+            var toDoList = _session.Get<List>();
+            toDoList.modify(rightleft, right);
 
             return FubuContinuation.RedirectTo<ToDoListInputModel>();
         }
 
         public FubuContinuation post_SortList(JsonResult inputModel)
         {
-            var tempFile = Path.GetTempFileName();
-           var listItems = inputModel.arrayToDo;
-           // Persist to file
-           File.WriteAllLines(tempFile, listItems);
+            var order = inputModel.arrayToDo;
 
-           File.Delete(path);
-           File.Move(tempFile, path);
+            var toDoList = _session.Get<List>();
+            toDoList.sort(order);
 
-           return FubuContinuation.RedirectTo<ToDoListInputModel>();
+            return FubuContinuation.RedirectTo<ToDoListInputModel>();
         }
 
     }
 
     public class ToDoListViewModel
     {
-        public string[] ToDoList { get; set; }
+        public List<string> ToDoList { get; set; }
     }
 
     public class ToDoListInputModel // this class represents the "/to" URL
     {
     }
 
-     public class ToDoListPostInputModel
+    public class ToDoListPostInputModel
     {
-        public string DeleteItem { get; set; }
-        public string CompareItem { get; set; }
-        public string ModItem { get; set; }
-
+        public string AddItem { get; set; }
     }
 
-     public class JsonResult  // this class represents the "/to" URL
-     {
-         public string[] arrayToDo { get; set; }
-     }
+    public class ToDoListDeletePostInputModel
+    {
+        public string DeleteItem { get; set; }
+    }
+
+    public class ToDoListEditPostInputModel
+    {
+        public string CompareItem { get; set; }
+        public string ModItem { get; set; }
+    }
+
+
+    public class JsonResult  // this class represents the "/to" URL
+    {
+        public string[] arrayToDo { get; set; }
+    }
 
 }
